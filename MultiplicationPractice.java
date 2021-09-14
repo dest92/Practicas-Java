28ff027df0a7e6e12b639cf8f06b2fe55e45eba3
Matías Acebal <acebalmatias2019@gmail.com>
2021-09-14 10:41:46 -0300

    Create MultiplicationPractice.java

---
+++ MultiplicationPractice.java
@@ -0,0 +1,31 @@
+// Este simple script permite practicar las tablas de multiplicar con el número que elijas. 
+
+import java.util.Scanner;
+
+public class Tabla {
+  public static void main() {
+    System.out.println("Programa para aprender las tablas de multiplicar");
+    Scanner console = new Scanner(System.in);
+    System.out.println("Introduzca el número con el que desea practicar");
+    int tabla = console.nextInt();
+    int resultado = 0;
+    for (int z = 1; z <= tabla || z <= 10; z++) {
+      System.out.println(z + "x" + tabla + " " + "= ¿?");
+      System.out.println("Introduzca su respuesta");
+      int respuesta = console.nextInt();
+      if ((z * tabla) == respuesta) {
+        resultado++;
+        System.out.println("Valor correcto");
+      } else {
+        System.out.println("Lo siento se ha equivocado. La respuesta correcta era " + (z * tabla));
+      }
+    }
+    if (tabla >= 10) {
+      System.out.println("Has acertado " + resultado + " veces de " + tabla);
+    }
+
+    if (tabla < 10) {
+      System.out.println("Has acertado " + resultado + " veces de 10 ");
+    }
+  }
+}
